0aca61a9222919494802b4f4a6baeff0cb7839cb
System Administrator <root@bass.net.au>
2013-06-13 22:41:30 +0930

    starting to play around with layout

--- insertcustomer.php
+++ insertcustomer.php
@@ -1,3 +1,12 @@
+<html>
+<Head>
+<Title>
+Home
+</title>
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<BODY>
+
 <?php
 include 'connect.php';
 
@@ -10,6 +19,23 @@ if (!mysqli_query($con,$sql))
   {
   die('Error: ' . mysqli_error($con));
   }
-printf ("New Record has id %d.\n", mysqli_insert_id($con));
+$customerid=mysqli_insert_id($con);
+printf ("New Record has id $customerid.\n");
+
+
+$result = mysqli_query($con,"select * from customer where cstid = $customerid");
+
+$row = mysqli_fetch_array($result);
+?>
+<TABLE class="table table-striped">
+<tr>
+<td><input name="cstFName" type="text" value="<?php echo $row['cstFName'];?>" maxlength="100" id="cstFName" /> 
+<input name="cstLName" type="text" value="<?php echo $row['cstLName'];?>" maxlength="100" id="cstLName" />
+</td>
+</tr>
+
+<?php
 mysqli_close($con);
 ?>
+</body>
+</html>
